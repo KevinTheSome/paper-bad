f192aa7393c21a53eae6cfafed8de07dfdb53d7c
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2024-03-26 16:46:54 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,js,html,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
